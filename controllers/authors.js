368034373e86df10ea0cce63a5f9e294275f89e0
SUDHA SREE YERRAMSETTY <sudhasreeyerramsetty@gmail.com>
2024-05-11 15:50:55 +0530

    Author Routes Setup

---
+++ controllers/authors.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author.js');
+
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/all.ejs', {authors: authors, searchOptions: req.query})
+    } catch {
+        res.redirect('/')
+    }                
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new.ejs', {author: new Author()});
+})
+
+router.post('/', async (req, res) => {
+    const author = new Author({ name: req.body.name })
+    // async-await code
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors`);
+    } catch {
+        res.render('authors/new', 
+            {
+                author: author,
+                errorMessage: 'Error creating Author'
+            }
+        )
+    }
+    // // non async-await code
+    // author.save((err, newAuthor) => {
+    //     if(err){
+    //         res.render('authors/new', 
+    //             {
+    //                 // author: author,
+    //                 errorMessage: 'Error creating Author'
+    //             }
+    //         )
+    //     }
+    //     else {
+    //         // res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    // })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const indexRoute = require('./controllers/index');
+const indexRouter = require('./controllers/index.js');
+const authorRouter = require('./controllers/authors.js');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts);
 app.set(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
@@ -20,6 +23,7 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose.'));
 
-app.use('/', indexRoute);
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);                     // this /authors will be appended to the path
 
 app.listen(process.env.PORT || 3000);
