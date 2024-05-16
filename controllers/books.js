12c8b2b72d85a2052d3f4a49bc48468009d9bb4b
SUDHA SREE YERRAMSETTY <sudhasreeyerramsetty@gmail.com>
2024-05-16 16:18:19 +0530

    File Upload to Database Setup

--- controllers/books.js
+++ controllers/books.js
@@ -2,18 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Author = require('../models/author.js');
 const Book = require('../models/book.js');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-const uploadPath = path.join('public', Book.coverImageBasePath)
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
+
+// // multer code
+// const path = require('path');
+// const fs = require('fs');
+// const multer = require('multer');
+// const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+// const uploadPath = path.join('public', Book.coverImageBasePath)
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) => {
+//         callback(null, imageMimeTypes.includes(file.mimetype))
+//     }
+// })
 
 router.get('/', async (req, res) => {
     let query = await Book.find()
@@ -38,33 +40,62 @@ router.get('/new', async (req, res) => {
    renderNewPage(res, new Book())
 })
 
-router.post('/', upload.single('coverImage'), async (req, res) => {
-    const coverImageFileName = (req.file != null) ? req.file.filename : null
+// // router.post that uses multer
+// router.post('/', upload.single('coverImage'), async (req, res) => {
+//     const coverImageFileName = (req.file != null) ? req.file.filename : null
+//     const book = new Book({
+//         title: req.body.title,
+//         author: req.body.author,
+//         publishDate: new Date(req.body.publishDate),     // string being converted back into date
+//         pageCount: req.body.pageCount,
+//         coverImageName: coverImageFileName,
+//         description: req.body.description
+//     })
+
+//     try {
+//         const newBook = await book.save()
+//         // res.redirect(books/${newBook.id})
+//         res.redirect('books')
+//     } catch {
+//         if(book.coverImageName != null){
+//             removeBookCover(book.coverImageName)
+//         }
+//         renderNewPage(res, book, true)
+//     }
+// })
+// // code to remove book covers for error uploads to file system when using multer
+// function removeBookCover(fileName){
+//     fs.unlink(path.join(uploadPath, fileName), err => {
+//         if(err) console.error(err)
+//     })
+// }
+
+// router.post that doesn't use multer
+router.post('/', async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),     // string being converted back into date
         pageCount: req.body.pageCount,
-        coverImageName: coverImageFileName,
         description: req.body.description
     })
-
+    saveCover(book, req.body.coverImage)
     try {
         const newBook = await book.save()
-        // res.redirect(`books/${newBook.id}`)
+        // res.redirect(books/${newBook.id})
         res.redirect('books')
     } catch {
-        if(book.coverImageName != null){
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res, book, true)
     }
 })
-
-function removeBookCover(fileName){
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if(err) console.error(err)
-    })
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+function saveCover(book, coverImageEncoded){
+    if(coverImageEncoded == null) return;
+    const cover = JSON.parse(coverImageEncoded)   // JSON string
+    if(cover != null && imageMimeTypes.includes(cover.type)){
+        book.coverImg = new Buffer.from(cover.data, 'base64')      // creating a new Buffer from cover.data and from 'base64' format
+        book.coverImgType = cover.type
+    }
 }
 
 async function renderNewPage(res, book, hasError = false){
--- models/book.js
+++ models/book.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-const path = require('path')
 
-const coverImageBasePath = 'uploads/bookCovers'
+// // included in code that is using multer
+// const path = require('path')
+// const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -24,7 +25,17 @@ const bookSchema = new mongoose.Schema({
         require: true,
         default: Date.now
     },
-    coverImageName: {   // actual image file itself will be stored on the server in the file system; here we are storing only name of the image
+    // // below property used when code uses multer
+    // coverImageName: {   // actual image file itself will be stored on the server in the file system; here we are storing only name of the image
+    //     type: String,
+    //     required: true
+    // },
+    // below two properties used when code does not use multer
+    coverImg: {
+        type: Buffer,
+        required: true
+    },
+    coverImgType: {
         type: String,
         required: true
     },
@@ -35,12 +46,20 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
-// virtual property derives it's value from real properties mentioned in schema above
+// // virtual property derives it's value from real properties mentioned in schema above - included in code using multer
+// bookSchema.virtual('coverImagePath').get(function(){  // not using arrow function since we need 'this' property to refer to the actual book itself
+//     if(this.coverImageName != null){
+//         return path.join('/', coverImageBasePath, this.coverImageName) // root path will be /public
+//     }
+// })
+
+// virtual property derives it's value from real properties mentioned in schema above - included in code that is not using multer
 bookSchema.virtual('coverImagePath').get(function(){  // not using arrow function since we need 'this' property to refer to the actual book itself
-    if(this.coverImageName != null){
-        return path.join('/', coverImageBasePath, this.coverImageName) // root path will be /public
+    if(this.coverImg != null && this.coverImgType != null){
+        return `data:${this.coverImgType};charset=utf-8;base64,${this.coverImg.toString('base64')}`  // data object allows us to take Buffer data and use that as the source for the image
     }
 })
 
 module.exports = mongoose.model('Book', bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
+// // included in code using multer
+// module.exports.coverImageBasePath = coverImageBasePath;
